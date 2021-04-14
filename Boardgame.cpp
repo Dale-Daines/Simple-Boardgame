97a24cf24ff3df620e5a6fbab34053153235bb6f
Dale-Daines <78124767+Dale-Daines@users.noreply.github.com>
2021-04-14 14:24:09 +1000

    Update Boardgame.cpp

--- Boardgame.cpp
+++ Boardgame.cpp
@@ -1,16 +1,3 @@
-/*********** Declaration*******
-I hereby certify that no part of this assignment has been copied from
-any other student’s work or from any other source. No part of the code
-has been written/produced for me by another person or copied from any
-other source.
-I hold a copy of this assignment that I can produce if the original is
-lost or damaged.
-**************************/
-
-//Dale Daines 
-//16998416
-//OOP Assignment 1
-
 //Game is similar to Hex
 //Objective is to be the first player to link their sides (Top to Bottom or Left to Right)
 
